23381b89be77b4e783cb2542236ab3ff0f9a26a0
student <student>
2018-07-03 20:26:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+/**
+ * Created by student on 03.07.2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int i1;
+        i1=0;
+        int i2;
+        i2=1;
+        int i3;
+        i3=2;
+        String s = "String";
+        String s1 = "String";
+        String s2 = "other String";
+        //  Arithmetic
+        int result;
+        result=i2+i1;
+        System.out.println(result);
+        i1++;
+        System.out.println(i1);
+        result=i2/ i3;
+        System.out.println(result);
+        ++i1;
+        System.out.println(i1);
+
+    }
+}
